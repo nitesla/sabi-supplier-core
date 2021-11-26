8465ff0e02bb8c1ec1ed2eddabb10881748b5727
Davyjones <ekenedavidson15@gmail.com>
2021-11-26 13:46:14 +0100

    merged muyiwa

--- src/main/java/com/sabi/suppliers/core/dto/request/SupplierLocationRequestDto.java
+++ src/main/java/com/sabi/suppliers/core/dto/request/SupplierLocationRequestDto.java
@@ -6,9 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 /**
  *
  * This class collects the request and map it to the entity class
@@ -22,12 +19,8 @@ public class SupplierLocationRequestDto {
 
     private Long id;
 
-    @NotNull(message = "Supplier ID can not be empty")
-    @Min(message = "Supplier ID can not be less than 0", value = 1)
     private Long supplierID;
 
-    @NotNull(message = "State ID can not be empty")
-    @Min(message = "State ID can not be less than 0", value = 1)
     private Long stateID;
 
 }
