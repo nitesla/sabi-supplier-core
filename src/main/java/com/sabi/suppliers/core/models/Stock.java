665ef2446f24c4c8e515bcdb39c4e5f04ded95e2
muyiwa-spinel <muyiwa.a@spinel.consulting>
2021-12-02 15:06:04 -0800

    stock update

---
+++ src/main/java/com/sabi/suppliers/core/models/Stock.java
@@ -0,0 +1,26 @@
+package com.sabi.suppliers.core.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sabi.framework.models.CoreEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Stock extends CoreEntity {
+
+    private Long supplyGoodId;
+    private LocalDateTime actionDate;
+    private String action;
+    private int initialQuantity;
+    private int quantity;
+    private int finalQuantity;
+    private Long userId;
+}
