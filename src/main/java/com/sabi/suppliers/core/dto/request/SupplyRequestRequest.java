7b96d354d49c3b8ff594d74fcd8c07d83c2cdec3
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-12-16 20:21:30 +0100

    +++++ fixes

--- src/main/java/com/sabi/suppliers/core/dto/request/SupplyRequestRequest.java
+++ src/main/java/com/sabi/suppliers/core/dto/request/SupplyRequestRequest.java
@@ -44,7 +44,6 @@ public class SupplyRequestRequest {
     private Long quantity;
     @NotNull(message = "Price  is required")
     private BigDecimal price;
-    @NotNull(message = "Warehouse Id is required")
     private Long warehouseId;
     @NotBlank(message = "Drop off address is required")
     private String dropOffAddress;
